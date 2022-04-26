08e6184a7bf3bda44070207d0a187edcb75cf165
Ezequiel Lutter <lutterezequiel@gmail.com>
2022-04-25 22:08:48 -0300

    se agregaron atributos constructores y metodos de actividad y actividad tipo

--- cazoApp_back-end/src/main/java/cazo/app/modelo/Actividad.java
+++ cazoApp_back-end/src/main/java/cazo/app/modelo/Actividad.java
@@ -1,5 +1,62 @@
 package cazo.app.modelo;
 
-public class Actividad {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Actividad")
+public class  Actividad implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="actividad_id")
+    private Long id;
+    @Column(name = "actividad_nombre", length = 50, nullable = false)
+    private String nombre;
+    @Column(name = "actividad_url", length = 255, nullable = false)
+    private String url;
+
+    public Actividad() {
+    }
+
+    public Actividad(Long id, String nombre, String url) {
+        this.id = id;
+        this.nombre = nombre;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Actividad{" + "id=" + id + ", nombre=" + nombre + ", url=" + url + '}';
+    }
 
 }
--- cazoApp_back-end/src/main/java/cazo/app/modelo/ActividadTipo.java
+++ cazoApp_back-end/src/main/java/cazo/app/modelo/ActividadTipo.java
@@ -1,5 +1,63 @@
 package cazo.app.modelo;
 
-public class ActividadTipo extends Actividad{
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ActividadTipo")
+public class ActividadTipo extends Actividad implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "actividadTipo_id")
+    private Long id;
+    @Column(name = "actividadTipo_desc", length = 255, nullable = false)
+    private String descripcion;
+    @Column(name = "actividadTipo_cupo", nullable = false)
+    private Integer cupo;
+
+    public ActividadTipo() {
+    }
+
+    public ActividadTipo(Long id, String descripcion, Integer cupo, String nombre, String url) {
+        super(id, nombre, url);
+        this.cupo = cupo;
+        this.descripcion = descripcion;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getCupo() {
+        return cupo;
+    }
+
+    public void setCupo(Integer cupo) {
+        this.cupo = cupo;
+    }
+
+    @Override
+    public String toString() {
+        return "ActividadTipo{" + "id=" + id + ", descripcion=" + descripcion + ", cupo=" + cupo + '}';
+    }
 
 }
